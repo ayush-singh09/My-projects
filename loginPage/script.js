48401054e5b494e68eddbbd61384f21fa66c6d95
Ayush Singh <142167916+ayush-singh09@users.noreply.github.com>
2023-10-19 16:43:06 +0530

    Update script.js (Authentication)

--- loginPage/script.js
+++ loginPage/script.js
@@ -6,30 +6,97 @@ const closeIcon2 = document.querySelector('.closeIcon2')
 const regOpt = document.querySelector('.reg-option')
 const logOpt = document.querySelector('.log-option')
 const intro = document.querySelector('.intro')
+const regBtn = document.querySelector('.reg-btn')
+const logBtn = document.querySelector('.login-btn')
+
+var flag = 0
 
 login.addEventListener('click',function(){
-    loginForm.classList.add('active')
+    if(flag==0)
+    {
+        loginForm.classList.add('active')
+        flag = 1
+    }    
     intro.classList.remove('active')
 })
 
 closeIcon1.addEventListener('click',function(){
+    flag = 0
     loginForm.classList.remove('active')
     intro.classList.add('active')
 })
 
 closeIcon2.addEventListener('click',function(){
+    flag = 0
     regForm.classList.remove('active')
     intro.classList.add('active')
 })
 
 regOpt.addEventListener('click',function(){
+    flag = 1
     loginForm.classList.remove('active')
     regForm.classList.add('active')
     intro.classList.remove('active')
 })
 
 logOpt.addEventListener('click',function(){
+    flag = 1
     loginForm.classList.add('active')
     regForm.classList.remove('active')
     intro.classList.remove('active')
 })
+
+regBtn.addEventListener('click',function(){
+    let username = document.querySelector('.reg-user').value
+    let email = document.querySelector('.reg-email').value
+    let password = document.querySelector('.reg-pass').value
+
+    if(username !== '' && email !== '' && password !== '')
+    {
+        const existingUser = localStorage.getItem(username)
+
+        if(existingUser !== null)
+        {
+            alert("Username already exists! Please try another one.")
+
+        }
+        else {
+            localStorage.setItem(username,password)
+            alert("User Registered")
+            clear()
+        }
+    }
+    else{
+        alert("Please Fill all Details!")
+    }
+})
+
+logBtn.addEventListener('click',function(){
+    let username = document.querySelector('.log-user').value
+    let password = document.querySelector('.log-pass').value
+
+    if(username !== '' && password !== '')
+    {
+        const storedPassword = localStorage.getItem(username)
+
+        if(storedPassword === password)
+        {
+            alert("Login Successful!")
+        }
+        else{
+            alert("Incorrect Password! Try Again.")
+        }
+    }
+    else {
+        alert("Please fill in the details!")
+    }
+})
+
+function clear()
+{
+    document.querySelector('.reg-user').value = ''
+    document.querySelector('.reg-email').value = ''
+    document.querySelector('.reg-pass').value = ''
+    document.querySelector('.log-user').value = ''
+    document.querySelector('.log-pass').value = ''
+}
